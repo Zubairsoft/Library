a23926c1f54d47fc7bcef8ef86e89eb0ab45d728
mohammed zubair <moh.775123554@gmail.com>
2022-01-27 15:42:15 +0300

    updata project file

--- js/language.js
+++ js/language.js
@@ -14,21 +14,41 @@ function lange(){
 var slaider=document.querySelector(".head-imges");
 var imag=['adv.jpg','bag.jpg','mobile.jpg','mobile2.jpg'];
 var ind=0 ;// current image index
+var dot=document.getElementsByClassName('dot');
 function Pv(){
+    
 
     if (ind<=0) ind=imag.length;
+
     ind--;
+    for(let a=0;a<dot.length;a++){
+
+        dot[a].classList.remove("active");
+    }
+    dot[ind].classList.add("active");
     return SetImg();
 }
 function Nt(){
+    
 
     if(ind>=imag.length-1) ind= -1;
+    
     ind++;
+    for(let a=0;a<dot.length;a++){
+
+        dot[a].classList.remove("active");
+    }
+    dot[ind].classList.add("active");
     return SetImg();
 }
 function SetImg(){
     return slaider.setAttribute('src','img/'+ imag[ind]);
+    
+    
 }
+
+
+
 // search
 const search=document.getElementById('search');
 const book_search=document.querySelectorAll('.slider-container-item');
@@ -58,5 +78,23 @@ card[b].addEventListener('click',(e)=>{
 
 )
 }
+// slider.index
+// const booksize = document.querySelector('.slider-container-item .books-container').clientWidth;
+const buttonRight = document.querySelectorAll('.slideRight');
+const buttonLeft = document.querySelectorAll('.slideLeft');
+
+buttonRight.forEach(element => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    element.parentElement.scrollLeft += 200;
+  });
+});
+
+buttonLeft.forEach(element => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    element.parentElement.scrollLeft -= 200;
+  });
+});
 
 
